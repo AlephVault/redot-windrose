f3b159da5256123b5b470866cbe6d754a4a37c86
Luis Masuelli <luisfmasuelli@gmail.com>
2025-04-08 13:50:43 -0500

    Adding the MapRuleSpec and EntityRuleSpec implementations

--- addons/AlephVault.WindRose/rules/entity_rule_spec.gd
+++ addons/AlephVault.WindRose/rules/entity_rule_spec.gd
@@ -1,1 +1,7 @@
 extends Resource
+
+## Instantiates a rule object for the given entity.
+func create_rule(entity) -> AlephVault__WindRose.Rules.EntityRule:
+	## TODO annotate: entity: AlephVault__WindRose.Entities.Entity
+	assert(false, "EntityRuleSpec::create_rule(entity) must be implemented")
+	return null
--- addons/AlephVault.WindRose/rules/map_rule_spec.gd
+++ addons/AlephVault.WindRose/rules/map_rule_spec.gd
@@ -1,1 +1,7 @@
 extends Resource
+
+## Instantiates a rule object for the given entity.
+func create_rule(entity) -> AlephVault__WindRose.Rules.MapRule:
+	## TODO annotate: entity: AlephVault__WindRose.Maps.Map
+	assert(false, "MapRuleSpec::create_rule(entity) must be implemented")
+	return null
