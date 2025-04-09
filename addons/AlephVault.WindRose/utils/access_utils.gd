98ee9a8e6b1bb796893fd36873a38ce5ab91939e
Luis Masuelli <luisfmasuelli@gmail.com>
2025-04-09 07:13:15 -0500

    Another utility method

--- addons/AlephVault.WindRose/utils/access_utils.gd
+++ addons/AlephVault.WindRose/utils/access_utils.gd
@@ -4,3 +4,8 @@ extends Object
 ## that are meant to not be settable.
 static func cannot_set(klass: String, property: String):
 	assert(false, "%s.%s cannot be set this way" % [klass, property])
+
+## This is a dummy utility to complain about methods
+## that are not implemented yet.
+static func not_implemented(klass: String, property: String):
+	assert(false, "%s.%s is not implemented" % [klass, property])
