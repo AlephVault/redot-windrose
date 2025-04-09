4229900fcbb08d3b4e3bc3e179e89f82d53cd205
Luis Masuelli <luisfmasuelli@gmail.com>
2025-04-08 22:02:57 -0500

    Adding the errors utils

---
+++ addons/AlephVault.WindRose/utils/error_utils.gd
@@ -0,0 +1,29 @@
+extends Object
+
+## This is some sort of exception or error code.
+class Error:
+	var _code: String
+
+	## The code. Typically, a slug.
+	var code: String:
+		get:
+			return _code
+		set(value):
+			AlephVault__WindRose.Utils.AccessUtils.cannot_set(
+				"Error", "code"
+			)
+	
+	var _detail: String
+
+	## The detail. Typically, a readable string.
+	var detail: String:
+		get:
+			return _detail
+		set(value):
+			AlephVault__WindRose.Utils.AccessUtils.cannot_set(
+				"Error", "detail"
+			)
+	
+	func _init(code: String, detail: String = ""):
+		_code = code
+		_detail = detail
