88e32cbb6252d139bf23d6025ad8fb51988fa7a3
Luis Masuelli <luisfmasuelli@gmail.com>
2025-04-14 22:06:08 -0500

    Moving the entities_rule.gd file and adding some constructors

---
+++ addons/AlephVault.WindRose/entities/entities_rule.gd
@@ -0,0 +1,19 @@
+extends AlephVault__WindRose.Rules.EntitiesRule
+## A Layer's EntitiesRule is an EntitiesRule that
+## holds a reference to the related layer.
+
+const _Layer = AlephVault__WindRose.Entities.Layer
+
+var _layer: _Layer
+
+## Returns the current layer.
+var layer: _Layer:
+	get:
+		return _layer
+	set(value):
+		AlephVault__WindRose.Utils.AccessUtils.cannot_set(
+			"EntitiesRule", "layer"
+		)
+
+func _init(layer: _Layer) -> void:
+	_layer = layer
--- addons/AlephVault.WindRose/entities/index.gd
+++ addons/AlephVault.WindRose/entities/index.gd
@@ -3,3 +3,4 @@ extends Object
 const Entity = preload("./entity.gd")
 const Layer = preload("./layer.gd")
 const EntityRule = preload("./entity_rule.gd")
+const EntitiesRule = preload("./entities_rule.gd")
