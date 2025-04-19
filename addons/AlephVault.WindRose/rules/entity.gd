e5cf71d868beb80416d87ab8187589acd18fc8c3
Luis Masuelli <luisfmasuelli@gmail.com>
2025-04-19 16:05:05 -0500

    Adding some trackings of position/movement in the entity itself

--- addons/AlephVault.WindRose/rules/entity.gd
+++ addons/AlephVault.WindRose/rules/entity.gd
@@ -42,15 +42,64 @@ var size: Vector2i:
 			"Entity", "size"
 		)
 
+var _cell: Vector2i
+
+## The current cell this entity is into. It will
+## be (-1, -1) if it's not attached to a map.
+var cell: Vector2i:
+	get:
+		return _cell
+	set(value):
+		AlephVault__WindRose.Utils.AccessUtils.cannot_set(
+			"Entity", "cell"
+		)
+
+var _movement: _Direction
+
+## The current movement direction. If it's NONE,
+## then the entity is not moving.
+var movement: _Direction:
+	get:
+		return _movement
+	set(value):
+		AlephVault__WindRose.Utils.AccessUtils.cannot_set(
+			"Entity", "direction"
+		)
+
 func _on_attached(manager: _EntitiesManager, position: Vector2i):
 	_manager = manager
+	_cell = position
 
 func _on_detached():
+	_cell = Vector2i(-1, -1)
+	_movement = _Direction.NONE
 	_manager = null
 
 func _on_property_updated(property: String, old_value, new_value):
 	if _manager != null:
 		manager.property_updated(_entity_rule, property, old_value, new_value)
+
+func _on_movement_started(
+	from_position: Vector2i, to_position: Vector2i, direction: _Direction
+):
+	_movement = direction
+
+func _on_movement_finished(
+	from_position: Vector2i, to_position: Vector2i, direction: _Direction
+):
+	_movement = _Direction.NONE
+	_cell = to_position
+
+func _on_movement_cancelled(
+	from_position: Vector2i, reverted_position: Vector2i,
+	direction: _Direction
+):
+	_movement = _Direction.NONE
+
+func _on_teleported(
+	from_position: Vector2i, to_position: Vector2i
+):
+	_cell = to_position
 
 func _init(entity_rule: _EntityRule):
 	if entity_rule == null or entity_rule.signals == null:
@@ -59,7 +108,22 @@ func _init(entity_rule: _EntityRule):
 		_entity_rule = entity_rule
 		_entity_rule.signals.on_attached.connect(_on_attached)
 		_entity_rule.signals.on_detached.connect(_on_detached)
+		_entity_rule.signals.on_movement_started.connect(_on_movement_started)
+		_entity_rule.signals.on_movement_finished.connect(_on_movement_finished)
+		_entity_rule.signals.on_movement_cancelled.connect(_on_movement_cancelled)
+		_entity_rule.signals.on_teleported.connect(_on_teleported)
 		_entity_rule.on_property_updated.connect(_on_property_updated)
+
+func _notification(what):
+	if what == NOTIFICATION_PREDELETE:
+		if entity_rule != null and entity_rule.signals != null:
+			_entity_rule.signals.on_attached.disconnect(_on_attached)
+			_entity_rule.signals.on_detached.disconnect(_on_detached)
+			_entity_rule.signals.on_movement_started.disconnect(_on_movement_started)
+			_entity_rule.signals.on_movement_finished.disconnect(_on_movement_finished)
+			_entity_rule.signals.on_movement_cancelled.disconnect(_on_movement_cancelled)
+			_entity_rule.signals.on_teleported.disconnect(_on_teleported)
+			_entity_rule.on_property_updated.disconnect(_on_property_updated)
 
 ## Attepts attachment to a manager.
 func attach_to(
@@ -80,12 +144,6 @@ func detach() -> _Response:
 	if response.is_successful():
 		return _Response.succeed(true)
 	return response
-
-## Returns the status from the current manager.
-func get_status() -> _EntitiesManager.EntityStatus:
-	if _manager == null or _entity_rule == null:
-		return null
-	return manager.get_status_for(_entity_rule)
 
 ## Tries to start a movement in a direction.
 func movement_start(direction: _Direction, continued: bool = false) -> _Response:
