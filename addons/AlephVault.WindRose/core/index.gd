14e7697eaf30b6d616b8529ca5ef770c732f8c32
Luis Masuelli <luisfmasuelli@gmail.com>
2025-04-21 21:01:25 -0500

    Adding the resources/ directory and moving the entity_rule / entities_rule factory

--- addons/AlephVault.WindRose/core/index.gd
+++ addons/AlephVault.WindRose/core/index.gd
@@ -1,8 +1,6 @@
 extends Object
 
 const EntitiesRule = preload("./entities_rule.gd")
-const EntitiesRuleSpec = preload("./entities_rule_spec.gd")
 const EntitiesManager = preload("./entities_manager.gd")
 const EntityRule = preload("./entity_rule.gd")
-const EntityRuleSpec = preload("./entity_rule_spec.gd")
 const Entity = preload("./entity.gd")
---
+++ addons/AlephVault.WindRose/resources/index.gd
@@ -0,0 +1,4 @@
+extends Object
+
+const EntitiesRuleSpec = preload("./entities_rule_spec.gd")
+const EntityRuleSpec = preload("./entity_rule_spec.gd")
