e91d689faa1debb0b2ec02d1874a196974007e85
Luis Masuelli <luisfmasuelli@gmail.com>
2025-07-08 16:48:33 -0500

    Removing the old /entities directory

--- addons/AlephVault.WindRose/entities/index.gd
+++
@@ -1,6 +0,0 @@
-extends Object
-
-const Entity = preload("./entity.gd")
-const Layer = preload("./layer.gd")
-const EntityRule = preload("./entity_rule.gd")
-const EntitiesRule = preload("./entities_rule.gd")
