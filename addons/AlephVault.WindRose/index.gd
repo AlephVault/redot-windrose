080f0d9c66bbebf615dc1c8e1255edd3db37a2d4
Luis Masuelli <luisfmasuelli@gmail.com>
2025-07-08 11:14:57 -0500

    Adding the reference to the engine

--- addons/AlephVault.WindRose/index.gd
+++ addons/AlephVault.WindRose/index.gd
@@ -2,6 +2,7 @@ class_name AlephVault__WindRose
 extends Object
 
 const Core = preload("./core/index.gd")
+const WREngine = preload("./engine/index.gd")
 const Utils = preload("./utils/index.gd")
 const Maps = preload("./maps/index.gd")
 const Entities = preload("./entities/index.gd")
