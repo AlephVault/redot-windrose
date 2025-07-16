25b0de25d278c67da7e6ff9feeab3725934b3ae8
Luis Masuelli <luisfmasuelli@gmail.com>
2025-07-15 22:18:36 -0500

    Ensuring the dummy example for entity rule uses the appropriate size from the entity

--- addons/AlephVault.WindRose/contrib/dummy/samples/sample-map-entity.gd
+++ addons/AlephVault.WindRose/contrib/dummy/samples/sample-map-entity.gd
@@ -3,5 +3,5 @@ extends AlephVault__WindRose.Maps.MapEntity
 
 func _create_rule() -> _EntityRule:
 	return AlephVault__WindRose.Contrib.Dummy.EntityRule.new(
-		Vector2i(1, 1), true
+		size, true
 	)
