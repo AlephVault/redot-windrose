07450707e1bb867039981d33eb2051b7632d95a4
Luis Masuelli <luisfmasuelli@gmail.com>
2025-08-05 18:09:10 -0500

    Finishing the (untested) entities rule

--- addons/AlephVault.WindRose/contrib/simple/entities_rule.gd
+++ addons/AlephVault.WindRose/contrib/simple/entities_rule.gd
@@ -62,6 +62,47 @@ var navigability_rule: AlephVault__WindRose.Contrib.Navigability.EntitiesRule:
 			"EntitiesRule", "navigability_rule"
 		)
 
+func initialize_global_data():
+	blocking_rule.initialize_global_data()
+	solidness_rule.initialize_global_data()
+	navigability_rule.initialize_global_data()
+
+func initialize_cell_data(cell: Vector2i) -> void:
+	blocking_rule.initialize_cell_data(cell)
+	navigability_rule.initialize_cell_data(cell)
+	
+func update_cell_data(cell: Vector2i) -> void:
+	blocking_rule.update_cell_data(cell)
+	navigability_rule.update_cell_data(cell)
+
+func can_move(
+	entity_rule: AlephVault__WindRose.Core.EntityRule,
+	position: Vector2i, direction: _Direction
+) -> bool:
+	return (
+		blocking_rule.can_move(
+			entity_rule.blocking_rule,
+			position, direction
+		) and
+		solidness_rule.can_move(
+			entity_rule.solidness_rule,
+			position, direction
+		) and
+		navigability_rule.can_move(
+			entity_rule.navigability_rule,
+			position, direction
+		)
+	)
+
+## Tells whether an entity can be attached
+## to the map. In this case, the entity has
+## the simple rule attached.
+func can_attach(
+	entity_rule: AlephVault__WindRose.Core.EntityRule,
+	cell: Vector2i
+) -> bool:
+	return entity_rule is AlephVault__WindRose.Contrib.Simple.EntityRule
+
 func _init(
 	layer: AlephVault__WindRose.Maps.Layers.EntitiesLayer,
 	_up_linked: Callable,
@@ -80,4 +121,75 @@ func _init(
 	)
 	_navigability_rule = AlephVault__WindRose.Contrib.Navigability.EntitiesRule.new(
 		layer
+	)
+
+func on_entity_attached(
+	entity_rule: AlephVault__WindRose.Core.EntityRule,
+	to_position: Vector2i
+) -> void:
+	solidness_rule.on_entity_attached(
+		entity_rule.solidness_rule,
+		to_position
+	)
+
+func on_movement_started(
+	entity_rule: AlephVault__WindRose.Core.EntityRule,
+	start_position: Vector2i, end_position: Vector2i, direction: _Direction,
+	stage: MovementStartedStage
+) -> void:
+	solidness_rule.on_movement_started(
+		entity_rule.solidness_rule,
+		start_position, end_position, direction, stage
+	)
+
+func on_movement_finished(
+	entity_rule: AlephVault__WindRose.Core.EntityRule,
+	start_position: Vector2i, end_position: Vector2i, direction: _Direction,
+	stage: MovementConfirmedStage
+) -> void:
+	solidness_rule.on_movement_finished(
+		entity_rule.solidness_rule,
+		start_position, end_position, direction, stage
+	)
+	neighbours_rule.on_movement_finished(
+		entity_rule.neighbours_rule,
+		start_position, end_position, direction, stage
+	)
+
+func on_movement_cancelled(
+	entity_rule: AlephVault__WindRose.Core.EntityRule,
+	start_position: Vector2i, reverted_position: Vector2i, direction: _Direction,
+	stage: MovementClearedStage
+) -> void:
+	solidness_rule.on_movement_cancelled(
+		entity_rule.solidness_rule,
+		start_position, reverted_position, direction, stage
+	)
+
+func on_teleported(
+	entity_rule: AlephVault__WindRose.Core.EntityRule,
+	from_position: Vector2i, to_position: Vector2i,
+	stage: TeleportedStage
+) -> void:
+	solidness_rule.on_teleported(
+		entity_rule.solidness_rule,
+		from_position, to_position, stage
+	)
+
+func on_property_updated(
+	entity_rule: AlephVault__WindRose.Core.EntityRule,
+	property: String, old_value, new_value
+) -> void:
+	solidness_rule.on_property_updated(
+		entity_rule.solidness_rule,
+		property, old_value, new_value
+	)
+
+func on_entity_detached(
+	entity_rule: AlephVault__WindRose.Core.EntityRule,
+	from_position: Vector2i
+) -> void:
+	solidness_rule.on_entity_detached(
+		entity_rule.solidness_rule,
+		from_position
 	)
