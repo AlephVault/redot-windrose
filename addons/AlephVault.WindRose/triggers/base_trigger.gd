b80062ab4ab280540911aa91a622f70aceea1026
Luis Masuelli <luisfmasuelli@gmail.com>
2025-08-22 17:54:47 -0500

    Adding the BaseTrigger new logic

--- addons/AlephVault.WindRose/triggers/base_trigger.gd
+++ addons/AlephVault.WindRose/triggers/base_trigger.gd
@@ -231,6 +231,44 @@ var __registry_running: bool = false
 # The elements in the registry.
 var __registry_elements: Dictionary = {}
 
+# This is a references count for the entities that
+# the registered elements are tracked for. Perhaps
+# two or more Node2D (body) objects / Area2D (area)
+# objects are attached to the same entity. In this
+# case, we don't want to track some events for the
+# same entity twice.
+var __registry_entities: Dictionary = {}
+
+# This record tracks the data associated to an element,
+# which is a body or area node overlapping with this
+# Area2D object. It refers the associated entity and
+# also has a callback to self-remove itself from this
+# registry when it's removed from the tree or somehow
+# unscoped from this trigger.
+class ElementRecord:
+	# The current entity for this object.
+	var entity: AlephVault__WindRose.Maps.MapEntity
+	
+	# The exited_tree associated with the element
+	# in the registry. When invoked, the element
+	# will be un-registered.
+	var exited_tree: Callable
+
+# This record tracks the data associated to an entity.
+# This means: an element exists in this registry but
+# also one or more elements are associated with some
+# entities, which can match (i.e. many elements to be
+# children of the same entity). For this reason, now
+# we track the entity with references count so we
+# don't process the entity twice. It has its own new
+# callback for on_movement_finished signal.
+class EntityRecord:
+	# The references count for this object.
+	var references_count: int
+	
+	# The on_movement_finished signal callback.
+	var on_movement_finished: Callable
+
 func __start_registry():
 	var areas: Array = get_overlapping_areas()
 	for area in areas:
@@ -246,13 +284,85 @@ func __stop_registry():
 		__remove_element(e)
 
 func __tick_all():
-	# TODO run the registry's tick for each entity.
-	pass
+	for e in __registry_entities.keys():
+		__tick(e)
+
+func __tick(e: AlephVault__WindRose.Maps.MapEntity):
+	if is_instance_valid(e):
+		__trigger_entity_staying(e)
 
 func __add_element(e: Node2D):
-	# TODO add the element to the registry.
-	pass
+	if not __registry_elements.has(e):
+		# First, create the record for this
+		# element being added.
+		var value = ElementRecord.new()
+		__registry_elements[e] = value
+
+		# Then, create a callback for this
+		# element when it's taken from the
+		# tree. Connect it to the signal of
+		# the element being the key.
+		var element_exited_tree = func():
+			__remove_element(e)
+		value.exited_tree = element_exited_tree
+		e.tree_exited.connect(value)
+		
+		# Now, in the lapse of this record
+		# existing, the object cannot change
+		# their parent entity: it either has
+		# or does not have an entity. So we
+		# perform the tracking of the entity
+		# (being attached, detached, moved
+		# and staying) right here.
+		var parent = e.get_parent()
+		if parent is AlephVault__WindRose.Maps.MapEntity and parent.current_map == _map_entity.current_map:
+			value.entity = parent
+			__track_entity(parent)
 
 func __remove_element(e: Node2D):
-	# TODO remove the element from the registry.
-	pass
+	if not __registry_elements.has(e):
+		return
+	var existing: ElementRecord = __registry_elements[e]
+	if is_instance_valid(e) and e.tree_exited.is_connected(existing.exited_tree):
+		e.tree_exited.disconnect(existing.exited_tree)
+	__registry_elements.erase(e)
+	if existing.entity != null:
+		__untrack_entity(existing.entity)
+
+func __track_entity(entity: AlephVault__WindRose.Maps.MapEntity):
+	if not __registry_entities.has(entity):
+		var value: EntityRecord = EntityRecord.new()
+		__registry_entities[entity] = value
+		__trigger_entity_entered(entity)
+		# We don't need callbacks for on_detached
+		# or on_attached in the object, since they
+		# trigger tree-exits / tree-enters each
+		# time. So they're already handled here.
+		#
+		# BUT we DO track the on_movement_finished
+		# signal.
+		var _on_movement_finished = func(
+			fp: Vector2i, tp: Vector2i, d: AlephVault__WindRose.Utils.DirectionUtils.Direction
+		):
+			__trigger_entity_moved(entity)
+		entity.rule.signals.on_movement_finished.connect(_on_movement_finished)
+		value.on_movement_finished = _on_movement_finished
+		
+	var existing: EntityRecord = __registry_entities[entity]
+	existing.references_count += 1
+
+func __untrack_entity(entity: AlephVault__WindRose.Maps.MapEntity):
+	if not __registry_entities.has(entity):
+		return
+	var existing: EntityRecord = __registry_entities[entity]
+	existing.references_count -= 1
+	if existing.references_count <= 0:
+		# Here, we just clear the entity.
+		__trigger_entity_left(entity)
+		__registry_entities.erase(entity)
+		if entity.rule.signals.on_movement_finished.is_connected(
+			existing.on_movement_finished
+		):
+			entity.rule.signals.on_movement_finished.disconnect(
+				existing.on_movement_finished
+			)
