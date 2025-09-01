5617568e6f82f35e10db46b7ec9bbf3812bf9aa3
Luis Masuelli <luisfmasuelli@gmail.com>
2025-08-31 21:32:52 -0500

    Starting the logic for the visual objects (MapEntityVisual)

--- addons/AlephVault.WindRose/maps/index.gd
+++ addons/AlephVault.WindRose/maps/index.gd
@@ -2,6 +2,7 @@ extends Object
 
 const Map = preload("./map.gd")
 const MapEntity = preload("./map_entity.gd")
+const MapEntityVisual = preload("./map_entity_visual.gd")
 const Layers = preload("./layers/index.gd")
 const World = preload("./world.gd")
 const Scope = preload("./scope.gd")
---
+++ addons/AlephVault.WindRose/maps/map_entity_visual.gd
@@ -0,0 +1,5 @@
+extends Sprite2D
+## A map entity visual is an image that has a custom
+## behaviour and is attached to an entity. Entities
+## can have many of these objects as representations
+## (e.g. character shapes, effects, overlays, ...).
