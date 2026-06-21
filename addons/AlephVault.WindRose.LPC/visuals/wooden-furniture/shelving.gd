0c7fc6d8412c4d5ba69247c1a513d72a96383bc4
Luis Masuelli <luisfmasuelli@gmail.com>
2026-06-20 21:50:26 -0500

    Adding even more furniture.

---
+++ addons/AlephVault.WindRose.LPC/visuals/wooden-furniture/shelving.gd
@@ -0,0 +1,14 @@
+@tool
+extends "./base.gd"
+
+
+const _REGION_RECT := Rect2i(320, 416, 32, 64)
+const _OFFSET := Vector2(0, -48)
+
+
+func _get_region_rect() -> Rect2i:
+	return _REGION_RECT
+
+
+func _get_offset() -> Vector2:
+	return _OFFSET
