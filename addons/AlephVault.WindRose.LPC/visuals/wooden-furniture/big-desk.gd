576aa3e35ea499d1f82c97c441ca87d4d938b587
Luis Masuelli <luisfmasuelli@gmail.com>
2026-06-21 09:53:55 -0500

    Adding big desks.

---
+++ addons/AlephVault.WindRose.LPC/visuals/wooden-furniture/big-desk.gd
@@ -0,0 +1,35 @@
+@tool
+extends "./base.gd"
+
+
+enum BigDeskType {
+	DESK_TYPE_1,
+	DESK_TYPE_2,
+}
+
+
+const _DESK_TYPE_REGION_RECTS := {
+	BigDeskType.DESK_TYPE_1: Rect2i(64, 704, 64, 64),
+	BigDeskType.DESK_TYPE_2: Rect2i(128, 704, 64, 64),
+}
+
+const _OFFSET := Vector2(0, -32)
+
+
+## The big desk visual variant.
+@export var desk_type: BigDeskType = BigDeskType.DESK_TYPE_1:
+	set(value):
+		if desk_type == value:
+			return
+		desk_type = value
+		_setup_sprite()
+
+
+func _get_region_rect() -> Rect2i:
+	return _DESK_TYPE_REGION_RECTS.get(
+		desk_type, _DESK_TYPE_REGION_RECTS[BigDeskType.DESK_TYPE_1]
+	)
+
+
+func _get_offset() -> Vector2:
+	return _OFFSET
---
+++ addons/AlephVault.WindRose.LPC/visuals/wooden-furniture/sided-big-desk.gd
@@ -0,0 +1,23 @@
+@tool
+extends "./base.gd"
+
+
+const _RIGHT_REGION_RECT := Rect2i(0, 704, 32, 96)
+const _LEFT_REGION_RECT := Rect2i(32, 704, 32, 96)
+const _OFFSET := Vector2(0, -32)
+
+
+func _get_region_rect() -> Rect2i:
+	return _RIGHT_REGION_RECT
+
+
+func _get_region_rect_left() -> Rect2i:
+	return _LEFT_REGION_RECT
+
+
+func _get_region_rect_right() -> Rect2i:
+	return _RIGHT_REGION_RECT
+
+
+func _get_offset() -> Vector2:
+	return _OFFSET
