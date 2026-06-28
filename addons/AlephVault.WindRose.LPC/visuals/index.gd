3ecf16b405bbef28395d71166c6268f62889d4dc
Luis Masuelli <luisfmasuelli@gmail.com>
2026-06-28 17:57:28 -0500

    Starting a package for victorian real estate.

--- addons/AlephVault.WindRose.LPC/visuals/index.gd
+++ addons/AlephVault.WindRose.LPC/visuals/index.gd
@@ -3,3 +3,4 @@ extends Object
 const Farm = preload("./farm/index.gd")
 const WoodenFurniture = preload("./wooden-furniture/index.gd")
 const VictorianStreetAppliances = preload("./victorian-street-appliances/index.gd")
+const VictorianRealEstate = preload("./victorian-real-estate/index.gd")
