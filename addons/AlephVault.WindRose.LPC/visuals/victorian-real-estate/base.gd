6b1a098eb02b04a068ac2064073d5e469b3d89fb
Luis Masuelli <luisfmasuelli@gmail.com>
2026-06-30 09:22:37 -0500

    Fixing the base.gd for victorian real estate, to set the texture appropriately.

--- addons/AlephVault.WindRose.LPC/visuals/victorian-real-estate/base.gd
+++ addons/AlephVault.WindRose.LPC/visuals/victorian-real-estate/base.gd
@@ -6,3 +6,5 @@ const _Context := AlephVault__WindRose.Utils.Textures.Context
 
 const TEXTURE_CACHE_KEY := "AlephVault.WindRose.LPC:victorian-real-estate"
 const _DEFAULT_CACHE_MAX_DISPOSAL_SIZE := 64
+
+const _SOURCE_TEXTURE := preload("res://addons/AlephVault.WindRose.LPC/images/victorian-decoration/mansion.png")
